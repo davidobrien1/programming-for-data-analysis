97f9dcf3bdf3f455c888f18455f3ccc6ad8a1d64
unknown <davidobrien1@outlook.com>
2018-09-27 23:41:12 +0100

    added titles and legend to 2dplot.py

--- matplotlib-examples/2dplot.py
+++ matplotlib-examples/2dplot.py
@@ -5,13 +5,14 @@ x = np.arange(0.0, 10.0, 0.01) # Numpy will give me a range of number between 0 
 y = 3.0 * x + 1.0 # x and y are arrays
 noise = np.random.normal(0.0, 1.0, len(x))
 
+plt.plot(x, y + noise, 'r.', label="Actual")  # "b." means blue dot in matplotlib
+plt.plot(x, y, 'b-', label="Model") # this plots another line to the plot
 
-plt.plot(x, y + noise, 'r.')  # "b." means blue dot in matplotlib
-plt.plot(x, y, 'b-') # this plots another line to the plot
+plt.title("Simple Plot")
+plt.xlabel("Weigth")
+plt.ylabel("Mass")
+plt.legend()
+
 
 plt.show()
 
-plt.plot(x, y + noise, 'c.')  # "b." means blue dot in matplotlib
-plt.plot(x, y, 'g-') # this plots another line to the plot
-
-plt.show()
