ded7649223d441fefaa2f6607bdbfdcda514c9cd
unknown <davidobrien1@outlook.com>
2018-09-27 22:30:28 +0100

    updated gcd file to include a function

--- gcd.py
+++ gcd.py
@@ -1,7 +1,14 @@
-a = 50
-b = 20
+def  gcd(a, b):
+    """
+    Returns the greatest common divisor of and b.
+    """
+    if a < b:
+        a, b = b,a
 
-while b > 0:
-    a, b = b, a % b
+    while b > 0:
+        a, b = b, a % b
 
-print(a)
+    return a
+
+print(gcd(50, 20))
+print(gcd(22,143))
